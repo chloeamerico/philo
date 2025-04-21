18231ce032f227a056e9481bc15632242609421b
Chloe Americo <camerico@bess-f2r3s7.clusters.42paris.fr>
2025-04-21 19:14:24 +0200

    on avance

--- srcs/routine.c
+++ srcs/routine.c
@@ -6,47 +6,50 @@
 /*   By: camerico <camerico@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/21 17:06:48 by camerico          #+#    #+#             */
-/*   Updated: 2025/04/21 18:13:32 by camerico         ###   ########.fr       */
+/*   Updated: 2025/04/21 19:08:02 by camerico         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-void routine(t_data *data)
+void routine(void *arg)
 {
-	int	i = 0;
+	t_philo	*philo = (t_philo *)arg;
+	t_data *data = philo->data;
 	
 	while (data->philo_death == 0) // tant qu'aucun philo n'est pas mort, on comtinue la simulation
 	{
 		//etape de penser
 		pthread_mutex_lock(&data->printf_mutex);
-		printf("%lg %d is thinking\n", get_time_in_ms(), &data->philo[i]->id);
+		printf("%ld %d is thinking\n", (get_time_in_ms() - data->start_time), philo->id);
 		pthread_mutex_unlock(&data->printf_mutex);
 
 		//etape de prendre les fourchettes
-		pthread_mutex_lock(data->philo->left_fork); // on bloque le mutex de la fourchette de gauche
+		pthread_mutex_lock(philo->left_fork); // on bloque le mutex de la fourchette de gauche
 			pthread_mutex_lock(&data->printf_mutex);// on bloque le mutex d'ecriture
-			printf("%lg %d X has taken a fork\n", get_time_in_ms(), &data->philo[i]->id);
+			printf("%ld %d has taken a fork\n", (get_time_in_ms() - data->start_time), philo->id);
 			pthread_mutex_unlock(&data->printf_mutex);
 			
-				pthread_mutex_lock(data->philo->right_fork); // on bloque le mutex de la fourchette de droite
-				&data.philo[i]->last_meal = get_time_in_ms();
+				pthread_mutex_lock(philo->right_fork); // on bloque le mutex de la fourchette de droite
+				philo->last_meal = get_time_in_ms();
 					pthread_mutex_lock(&data->printf_mutex);// on bloque le mutex d'ecriture
-					printf("%lg %d X has taken a fork\n", get_time_in_ms(), &data->philo[i]->id);
+					printf("%ld %d has taken a fork\n", (get_time_in_ms() - data->start_time), philo->id);
 					pthread_mutex_unlock(&data->printf_mutex);
 					if (data->nb_of_meals_required != -1)
-						&data.philo[i]->meals_count++;
+						philo->meals_count++;
 		
 						pthread_mutex_lock(&data->printf_mutex);// on bloque le mutex d'ecriture
-						printf("%lg %d X is eating\n", get_time_in_ms(), &data->philo[i]->id);
+						printf("%ld %d is eating\n", (get_time_in_ms() - data->start_time), philo->id);
 						pthread_mutex_unlock(&data->printf_mutex);
+						usleep(data->time_to_eat * 1000);
 						
-		pthread_mutex_unlock(data->philo->left_fork);
-		pthread_mutex_unlock(data->philo->right_fork);
+		pthread_mutex_unlock(philo->left_fork);
+		pthread_mutex_unlock(philo->right_fork);
 
 		//etape pour dormir
 		pthread_mutex_lock(&data->printf_mutex);
-		printf("%lg %d is sleeping\n", get_time_in_ms(), &data->philo[i]->id);
+		printf("%ld %d is sleeping\n", (get_time_in_ms() - data->start_time), philo->id);
 		pthread_mutex_unlock(&data->printf_mutex);
+		usleep(data->time_to_sleep * 1000);
 	}
 }
